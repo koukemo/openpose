cb09b3c9f977a24cb8932c8b3193847beb389754
koukemo <java.sparrow22.surface@gmail.com>
2022-05-19 18:43:38 +0900

    [Fix] Shigure_openposeの修正テスト

--- python/openpose/openpose_python.cpp
+++ python/openpose/openpose_python.cpp
@@ -370,7 +370,7 @@ namespace op
             .def_readwrite("cvOutputData", &Datum::cvOutputData)
             .def_readwrite("cvOutputData3D", &Datum::cvOutputData3D)
             //.def_readwrite("poseKeypoints", &Datum::poseKeypoints)
-            .def("poseKeypoints", &Datum::getPoseKeypoints)
+            .def_readonly("poseKeypoints", &Datum::getPoseKeypoints)
             .def_readwrite("poseIds", &Datum::poseIds)
             .def_readwrite("poseScores", &Datum::poseScores)
             .def_readwrite("poseHeatMaps", &Datum::poseHeatMaps)
